1ba2d9d014ad967c496ce8ab36ddae94943377e0
ZeynebR <zeynabnr@code.edu.az>
2023-10-19 07:48:16 +0200

    task added

---
+++ Shoop.App/Program.cs
@@ -0,0 +1,74 @@
+﻿using Shoop.Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+Product[] products = { };
+
+Console.WriteLine("1. Create product");
+Console.WriteLine("2. Show products");
+Console.WriteLine("0. Close");
+string request = Console.ReadLine();
+
+while (request != "0")
+{
+    switch (request)
+    {
+        case "1":
+            CreateProduct();
+            break;
+
+        case "2":
+            ShowProduct();
+            break;
+
+        default:
+            Console.WriteLine("Please add valid option");
+            break;
+    }
+
+    Console.WriteLine("1. Create product");
+    Console.WriteLine("2. Show products");
+    Console.WriteLine("0. Close");
+    request = Console.ReadLine();
+
+}
+
+void CreateProduct()
+{
+    Console.WriteLine("Please enter product name");
+    string Name = ValidateString(Console.ReadLine());
+    Console.WriteLine("Please enter product price");
+    double Price = double.Parse(Console.ReadLine());
+    Console.WriteLine("Please enter product category");
+    string Category = ValidateString(Console.ReadLine());
+    Console.WriteLine("Please enter product stock count");
+    int Stockcount = int.Parse(Console.ReadLine());
+
+    Product product = new Product(Name, Price, Category, Stockcount);
+    Array.Resize(ref products, products.Length + 1);
+    products[products.Length - 1] = product;
+
+    Console.WriteLine("Product created");
+
+}
+
+void ShowProduct()
+{
+    foreach (Product product in products)
+    {
+        Console.WriteLine(product.GetFullInfo());
+
+    }
+}
+
+string ValidateString(string data)
+{
+    while (string.IsNullOrWhiteSpace(data))
+    {
+        Console.WriteLine("Please enter a valid value");
+        data = Console.ReadLine();
+    }
+    return data;
+}
+
+
+
---
+++ Shoop.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Shoop.Core.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public int StockCount { get; set; }
+
+        public Product(string name, double price, string category, int stockcount)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Category = category;
+            this.StockCount = stockcount;
+        }
+
+
+
+        public string GetFullInfo()
+        {
+            return $"Name: {Name} Price: {Price} Category {Category} StockCount {StockCount}";
+        }
+    }
+
+
+}
